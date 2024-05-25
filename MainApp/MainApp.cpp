a70eb1de5e1851fe8821f38aee9b0f7ff90422c0
Yimaga Iori <964302865@qq.com>
2024-05-26 01:09:51 +0800

    添加项目文件。

---
+++ MainApp/MainApp.cpp
@@ -0,0 +1,18 @@
+﻿// MainApp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <windows.h>
+
+int main() {
+	typedef void (*MyFunc)(void);
+	MyFunc foo = NULL;
+	HMODULE hdll = LoadLibraryA("MainDll.dll");
+	if (hdll != NULL) {
+		foo = (MyFunc)GetProcAddress(hdll, "MyFunc");
+	}
+	if (foo != NULL) {
+		(*foo)();
+	}
+	system("pause");
+	return 0;
+}
